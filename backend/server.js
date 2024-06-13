3403c4c5994358630460b245fb1a9c131f096910
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2024-06-13 16:02:30 +0530

    Cors added while creating server

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,21 @@ import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import homeRoutes from "./src/routes/home.routes.js";
+import cors from "cors";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+
+
 let port = 8080;
 
+app.use(cors());
 app.use(homeRoutes);
 
 io.on("connection", (socket) => {
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,24 @@
+import { socket } from "../../socket";
+import { useState, useEffect } from "react";
 const Home = () => {
-    return <div>Home</div>;
+    const [isConnected, setIsConnected] = useState(false);
+
+    const onConnect = () => {
+        setIsConnected(true);
+    };
+
+    useEffect(() => {
+        socket.on("connect", onConnect);
+        return () => {
+            socket.off("connect", onConnect);
+        };
+    }, []);
+
+    return (
+        <div>
+            <p> Is Connected - {isConnected ? "yes" : "no"}</p>
+        </div>
+    );
 };
 
 export default Home;
