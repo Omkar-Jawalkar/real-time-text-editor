2000be7855694e14e34acfc21bc95e201508d179
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2024-06-16 18:05:27 +0530

    States in home page and validations added

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,8 +6,34 @@ import {
     Button,
     Heading,
     Divider,
+    useToast,
 } from "@chakra-ui/react";
+import { useState } from "react";
+import { useEffect } from "react";
+
 const Home = () => {
+    const [roomId, setRoomId] = useState("");
+    const [username, setUsername] = useState("");
+    const toast = useToast();
+
+    const invalidUsernameOrRoomIdToast = () =>
+        toast({
+            title: "Please Fill complete information",
+            status: "warning",
+            duration: 5000,
+            isClosable: true,
+        });
+
+    const isUsernameAndRoomIdValid = () => {
+        if (username.length > 0 && roomId.length > 0) {
+            return true;
+        }
+        invalidUsernameOrRoomIdToast();
+        return false;
+    };
+
+    const handleJoinRoom = () => {};
+
     return (
         <Flex
             margin={"auto"}
@@ -23,9 +49,28 @@ const Home = () => {
             <Heading textAlign={"center"} size={"md"}>
                 Welcome to Shareditor
             </Heading>
-            <Input variant="filled" placeholder="Room Id"></Input>
-            <Input variant="filled" placeholder="Username"></Input>
-            <Button variant={"outline"}>Join</Button>
+            <Input
+                value={roomId}
+                onChange={(e) => setRoomId(e.target.value)}
+                variant="filled"
+                placeholder="Room Id"
+            />
+            <Input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                variant="filled"
+                placeholder="Username"
+            />
+            <Button
+                onClick={() => {
+                    if (isUsernameAndRoomIdValid()) {
+                        handleJoinRoom();
+                    }
+                }}
+                variant={"outline"}
+            >
+                Join
+            </Button>
             <Divider paddingY={2} />
             <Text textAlign={"center"}>
                 Dont have a Room Id? <Link color={"teal"}> Create one </Link>{" "}
