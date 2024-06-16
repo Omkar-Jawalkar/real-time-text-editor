ad0cf13b9db9d6a9ed086868763854d3db60326e
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2024-06-16 19:38:14 +0530

    Created fetchUsersInRoom, but code remaining

--- frontend/src/pages/room/Room.jsx
+++ frontend/src/pages/room/Room.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 const Room = () => {
     const { roomId } = useParams();
+
+    const fetchUsersInRoom = () => {};
+
+    useEffect(() => {
+        fetchUsersInRoom();
+    }, []);
 
     return <div>Room - {roomId}</div>;
 };
