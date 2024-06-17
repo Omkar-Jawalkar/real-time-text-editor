32ae813158f12fd0d4b041e696161fbfd77f1716
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2024-06-17 19:44:42 +0530

    Recoil added

--- frontend/src/pages/room/Room.jsx
+++ frontend/src/pages/room/Room.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { Flex } from "@chakra-ui/react";
 import EditorSection from "./EditorSection";
 import UsersSection from "./UsersSection";
+import { RecoilRoot } from "recoil";
 
 const Room = () => {
     const { roomId } = useParams();
@@ -26,10 +27,12 @@ const Room = () => {
     }, []);
 
     return (
-        <Flex minH={"inherit"}>
-            <EditorSection />
-            <UsersSection users={users} />
-        </Flex>
+        <RecoilRoot>
+            <Flex minH={"inherit"}>
+                <EditorSection />
+                <UsersSection users={users} />
+            </Flex>
+        </RecoilRoot>
     );
 };
 
