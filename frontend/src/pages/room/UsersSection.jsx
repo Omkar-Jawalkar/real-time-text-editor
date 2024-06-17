5044f5b0e46969f3bfbe3d193f7e5c81f768200f
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2024-06-17 19:56:51 +0530

    Recoil state set for user

--- frontend/src/pages/room/UsersSection.jsx
+++ frontend/src/pages/room/UsersSection.jsx
@@ -1,8 +1,11 @@
 import { Flex } from "@chakra-ui/react";
 import User from "./User";
-import { users } from "../../constants/users";
+// import { users } from "../../constants/users";
+import usersState from "../../atom/UsersState";
+import { useRecoilState } from "recoil";
 
 const UsersSection = () => {
+    const [users] = useRecoilState(usersState);
     return (
         <Flex flexDirection={"column"} flex={2} padding={4}>
             Connected Users :
