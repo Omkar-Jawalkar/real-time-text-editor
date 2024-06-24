e3cd3bd6aa5553ed956be6f45ac5e4b34f7dc1b4
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2024-06-24 20:49:25 +0530

    Production updated

--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 const URL =
     process.env.NODE_ENV === "production"
-        ? "http://localhost:8080"
+        ? "https://real-time-editor-backend.onrender.com"
         : "http://localhost:8080";
 
 export const socket = io(URL);
