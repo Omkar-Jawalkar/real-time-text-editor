92461f650c91c7560c23ab5d8551c3e3cdb6ae37
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2024-06-24 20:06:59 +0530

    changed production URL http://localhost:8080

--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 const URL =
-    process.env.NODE_ENV === "production" ? undefined : "http://localhost:8080";
+    process.env.NODE_ENV === "production"
+        ? "http://localhost:8080"
+        : "http://localhost:8080";
 
 export const socket = io(URL);
